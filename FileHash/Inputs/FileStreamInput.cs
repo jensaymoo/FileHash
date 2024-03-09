f405c898463eca1c2d960d461c1ac7b45d882f98
pigonthewind <pigonthewind@pm.me>
2024-03-09 19:28:53 +0300

    удалил лишний trycatch

--- FileHash/Inputs/FileStreamInput.cs
+++ FileHash/Inputs/FileStreamInput.cs
@@ -6,16 +6,11 @@
 
         public async Task<Stream> GetStream()
         {
-            try
+            return await Task.Run(() =>
             {
                 configuration = provider.GetConfiguration(new ConfigurationFileStreamValidator());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Configuration validation failed", ex);
-            }
-
-            return new FileStream(configuration.FileName, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 16384, useAsync: true);
+                return new FileStream(configuration.FileName, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 16384, useAsync: true);
+            });
         }
 
     }
